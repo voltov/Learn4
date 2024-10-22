b8f3271ab7b635ab5c5e7d4e5fcbc996c7f0621b
Volodymyr_Voitovych <Volodymyr_Voitovych@epam.com>
2024-10-22 22:41:03 +0300

    minor changes

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter line of text or type 'exit' to quit:");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        throw new Exception("Input can not be empty.\n");
+                    }
+
+                    Console.WriteLine($"First character of that line is: {input[0]}\n");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Task2/NumberParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException("Input string cannot be null.");
+            }
+
+            stringValue = stringValue.Trim();
+
+            if (string.IsNullOrEmpty(stringValue))
+            {
+                throw new FormatException("Input string cannot be empty.");
+            }
+
+            int result = 0;
+            bool isNegative = false;
+            int startIndex = 0;
+
+            if (stringValue[0] == '-')
+            {
+                isNegative = true;
+                startIndex = 1;
+            }
+            else if (stringValue[0] == '+')
+            {
+                startIndex = 1;
+            }
+
+            for (int i = startIndex; i < stringValue.Length; i++)
+            {
+                char c = stringValue[i];
+                if (c < '0' || c > '9')
+                {
+                    throw new FormatException("Input string was not in a correct format.");
+                }
+
+                if (isNegative)
+                {
+                    result = checked(result * 10 - (c - '0'));
+                }
+                else
+                {
+                    result = checked(result * 10 + (c - '0'));
+                }
+            }
+
+            return result;
+        }
+    }
+}
